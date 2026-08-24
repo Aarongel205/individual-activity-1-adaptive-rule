8c97c8099d17a59278c7683c749d437bce906884
Aarongel205 <aaronadanza92@gmail.com>
2026-08-24 18:57:25 +0800

    added appliances component

---
+++ frontend/src/components/Appliances.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import { products } from '../data/products.js'
+
+const Appliances = () => {
+
+  const getAppliancesProduct = products.filter(product => {
+    return product.category === 'appliances'
+  })
+
+  const appliancesProducts = getAppliancesProduct.map((product, index) => {
+    return (
+      <li
+        className='
+          shrink-0
+          w-32
+          sm:w-36
+          md:w-40
+          lg:w-44
+          h-48
+          sm:h-52
+          md:h-56
+          border-5
+        border-[#5A189A] 
+          rounded-lg
+          overflow-hidden
+          bg-[#5A189A] 
+          text-white
+        '
+        key={index}
+      >
+        <img
+          className='
+            w-full
+            h-28
+            sm:h-32
+            md:h-36
+            object-cover
+          '
+          src={product.imageUrl}
+          alt={product.name}
+        />
+
+        <div className='p-2'>
+          <p className='text-sm sm:text-base font-medium truncate'>
+            {product.name}
+          </p>
+
+          <p className='text-sm sm:text-base'>
+            {product.price}$
+          </p>
+        </div>
+      </li>
+    )
+  })
+
+  return (
+    <ul
+      className='
+        w-full
+        flex
+        flex-row
+        gap-2
+        sm:gap-3
+        overflow-x-auto
+        overflow-y-hidden
+        p-1
+        mb-4
+      '
+    >
+      {appliancesProducts}
+    </ul>
+  )
+}
+
+export default Appliances
